61bc138de2504cf9b9fa801a8e58712667808831
Menaka Narayanan <menaka@Menakas-MacBook-Air.local>
2017-11-26 20:51:03 -0500

    stores fall/spring and grad/undergrad

--- load.py
+++ load.py
@@ -8,7 +8,11 @@ a = 'a'
 t = 't'
 
 courses_to_q = dict()
-
+fall = set()
+spring = set()
+unknown = set()
+undergrad = set()
+grad = set()
 
 def helper(s):
 	print "S is {}".format(s)
@@ -17,8 +21,8 @@ def helper(s):
 	else:
 		return float(s)
 
-
-def store(filename):
+# Makes 
+def storeQdata(filename):
 	with open(filename, 'r') as f:
 		reader = csv.reader(f, delimiter = ",")
 		first = True
@@ -27,13 +31,37 @@ def store(filename):
 			if first:
 				first = False
 				continue
+
+			# Stores Q-scores
 			course = row[0]
 			courses_to_q[course] = dict()
 			courses_to_q[course][q] = helper(row[2])
 			courses_to_q[course][w] = helper(row[3])
 			courses_to_q[course][a] = helper(row[4])
 			courses_to_q[course][t] = row[5]
-		print courses_to_q
 
-store('courses.csv')
+			# Stores Fall or Spring class
+			if row[1] == "Fall":
+				fall.add(course)
+			elif row[1] == "Spring":
+				spring.add(course)
+			else:
+				unknown.add(course)
+
+			# Stores Grad versus Undergrad class
+			name = course.split()
+			if name[1][0] == '2' and len(name[1]) > 2:
+				grad.add(course)
+			else:
+				undergrad.add(course)
+
+
+		print "Q-scores for courses are {}".format(courses_to_q)
+		print "Fall classes are {}".format(fall)
+		print "Spring classes are {}".format(spring)
+		print "Unknown semester classes are {}".format(unknown)
+		print "Grad classes are {}".format(grad)
+		print "Undergrad classes are {}".format(undergrad)
+
+storeQdata('courses.csv')
 			
